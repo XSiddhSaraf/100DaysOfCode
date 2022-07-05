f39463c39c6e8ac3d672e5d9ebfac03e137ef3b1
Siddh <xpress.siddharth@gmail.com>
2022-07-06 03:26:49 +0530

    updated

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 # Calculator app
-
+from art1 import calculator_logo
 # Add
+
+
 def add(n1, n2):
     return n1 + n2
 
@@ -25,15 +27,32 @@ def div(n1, n2):
 
 operations = {'+': add, '-': sub, '*': mul, '/': div}
 
-num1 = int(input('What is the first number? '))
 
-for key in operations:
-    print(key)
-operation_symbol = input('Pick one operation from above list.')
-num2 = int(input('What is the second number? '))
+def calculator():
+    calculator_logo
+    num1 = int(input('What is the first number? '))
 
-calculation_function = operations[operation_symbol]
+    for key in operations:
+        print(key)
 
-answer = calculation_function(num1, num2)
+    should_continue = True
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    while should_continue:
+        operation_symbol = input('Pick one operation ')
+        num2 = int(input('What is the next number? '))
+
+        calculation_function = operations[operation_symbol]
+
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type y to continue with the {answer} or n to start a new calculation ") == 'y':
+            num1 = answer
+
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
